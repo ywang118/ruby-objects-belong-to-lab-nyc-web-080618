ffd31474730d65517e2bc81f49aa957ac80e9d65
Yixuan Wang <ywang0506@gmail.com>
2018-08-08 23:57:17 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,19 @@
+class Artist
+
+  attr_accessor :name
+
+
+end
+
+class Author
+
+  attr_accessor :name
+end
+
+class Post
+  attr_accessor :title, :author
+end
+
+class Song
+  attr_accessor :title, :artist
+end
